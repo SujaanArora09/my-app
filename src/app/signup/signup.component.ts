3ddccf1d8541773f388cc57299d7a5d2a976ad12
SujanAroraSLK <arorasujaan@gmail.com>
2025-01-08 15:14:50 +0530

    signup works
    signup works

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import axios from 'axios';
 
 @Component({
   selector: 'app-signup',
@@ -13,16 +14,31 @@ export class SignupComponent {
 
   constructor(private fb: FormBuilder) {
     this.signupForm = this.fb.group({
-      name: ['', Validators.required],
+      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
+      mobile: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      address: ['', Validators.required],
+      gender: ['', Validators.required],
     });
   }
 
   onSubmit() {
     if (this.signupForm.valid) {
-      console.log(this.signupForm.value);
-    }else {
+      const signupData = this.signupForm.value;
+      console.log('Sending signup data:', signupData);
+
+      axios
+        .post('http://localhost:8080/api/auth/signup', signupData)
+        .then((response) => {
+          console.log('Signup successful:', response.data);
+          alert('Signup successful!');
+        })
+        .catch((error) => {
+          console.error('Signup failed:', error);
+          alert('Signup failed. Please try again.');
+        });
+    } else {
       console.log('Form is invalid');
     }
   }
